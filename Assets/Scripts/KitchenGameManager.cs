cfe570b7d0bfeb6519ed48515038281d0989261c
inaciomaio <inacio.r.maio@gmail.com>
2025-01-24 19:28:30 +0000

    Lecture 39. Pause, Clear Statics

--- Assets/Scripts/KitchenGameManager.cs
+++ Assets/Scripts/KitchenGameManager.cs
@@ -7,6 +7,8 @@ public class KitchenGameManager : MonoBehaviour {
     public static KitchenGameManager Instance { get; private set; }
 
     public event EventHandler OnStateChanged;
+    public event EventHandler OnGamePaused;
+    public event EventHandler OnGameUnpaused;
 
     private enum State {
         WaitingToStart,
@@ -19,11 +21,20 @@ public class KitchenGameManager : MonoBehaviour {
     private float waitingToStartTimer = 1f;
     private float CountdownToStartTimer = 3f;
     private float gamePlayingTimer;
-    private float gamePlayingTimerMax = 10f;
+    private float gamePlayingTimerMax = 60 * 3f;
+    private bool isGamePaused = false;
 
     private void Awake() {
         Instance = this;
         state = State.WaitingToStart;
+    }
+
+    private void Start() {
+        GameInput.Instance.OnPauseAction += GameInput_OnPauseAction;
+    }
+
+    private void GameInput_OnPauseAction(object sender, EventArgs e) {
+        TogglePauseGame();
     }
 
     private void Update() {
@@ -76,5 +87,18 @@ public class KitchenGameManager : MonoBehaviour {
 
     public float GetGamePlayingTimerNormalized() {
         return 1 - (gamePlayingTimer / gamePlayingTimerMax);
+    }
+
+    public void TogglePauseGame() {
+        isGamePaused = !isGamePaused;
+        if (isGamePaused) {
+            Time.timeScale = 0f;
+            OnGamePaused?.Invoke(this, EventArgs.Empty);
+        }
+        else {
+            Time.timeScale = 1f;
+            OnGameUnpaused?.Invoke(this, EventArgs.Empty);
+        }
+
     }
 }
